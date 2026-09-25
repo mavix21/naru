9a5ecbd1104b9ff7cc0a13083c8669cbbff6c0c4
mavix21 <marcevizcarra2109@hotmail.com>
2026-09-24 23:41:26 -0500

    feat: update dependencies and improve UI components with style adjustments

--- app/src/components/ui/label.tsx
+++ app/src/components/ui/label.tsx
@@ -1,24 +1,19 @@
-"use client";
+"use client"
 
-import { cn } from "cn";
-import * as React from "react";
+import * as React from "react"
+import { cn } from "cn"
 
-function Label({
-  className,
-  htmlFor,
-  ...props
-}: React.ComponentProps<"label"> & { htmlFor: string }) {
+function Label({ className, ...props }: React.ComponentProps<"label">) {
   return (
     <label
-      htmlFor={htmlFor}
       data-slot="label"
       className={cn(
         "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
-        className,
+        className
       )}
       {...props}
     />
-  );
+  )
 }
 
-export { Label };
+export { Label }
